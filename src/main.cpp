93fab6cc6c22a71f841970c2758c897a04c09b96
Mathis <mathis.benoit@hotmail.com>
2025-01-16 15:52:04 +0100

    vrai commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,58 +1,67 @@
 /*
-Code créer par Benoit BOREK
-Modifié par Mathis Benoit
-dernière modification : 16/01/2025
+Nom du programme : WIZ811MJ ethernet
+Par Mathis Benoit
+Editeur : Visual Studio Code v1.96.2
+IDE : PlatformIO v3.3.4
 
+Dernière modification : 16/01/2025
 
-IDE : Visual Studio Code ver 1.96.2
+Matériel : 
+  - Module ethernet WIZ811MJ
+  - Carte Arduino Mega 
 
-Constituants :
-
-Arduino Mega 2560
-Module ethernet WIZ811MJ
-
-
-Lien de la doc technique du module : https://docs.wiznet.io/img/products/wiz811mj/WIZ811MJ_DS_V120E.pdf
+Câblage Module Arduino --> Module ethernet : 
+  - 3.3V --> 3V3D (J1:12)
+  - GND --> GND (J1:11)
+  - 50 (MISO) --> MISO (J1:2)
+  - 51 (MOSI) --> MOSI (J1:1)
+  - 52 (SCLK) --> SCLK (J2:3)
+  - 53 (CS) --> /SCS (J2:4)
 */
 
-
-
+#include <Arduino.h>    //Appel des librairies 
 #include <SPI.h>
 #include <Ethernet.h>
-#include <DHT.h>
+#include <EthernetUdp.h>
 
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
-IPAddress ip(192,168,1,20);
-IPAddress myDns(192, 168, 1, 1);
-IPAddress gateway(192, 168, 1, 1);
-IPAddress subnet(255, 255, 255, 0);
+#define   UDPMessagingPort    9999          // Selection du port UDP
 
+EthernetUDP Udp;
 
-
-
-void setup() {
-
+void setup(){
+  Serial.begin(9600);                     //Initialise le moniteur série à 9600 Bauds.
   
+  byte mac[] = { 0x0a, 0x00, 0x27, 0x00, 0x00, 0x10 };    //Adresse MAC du PC  (sur mon partage de connection)
+  byte ip[] = { 10, 186, 203, 64 };                       //Adresse IP du PC 
 
-  Serial.begin(9600);
-  Ethernet.init(10);
-  Serial.println("Initialisation Ethernet...");
+  while (!Serial) {
+    ; // Met en pause le programme le temps que la connection série soit établie 
+  }
 
-  // Initialisation de l'Ethernet en utilisant la pin 53 pour CS
-  Ethernet.begin(mac, ip, myDns, gateway, subnet);
-
+  // Vérifie si le module est bien connecté 
   if (Ethernet.hardwareStatus() == EthernetNoHardware) {
-    Serial.println("Le module Ethernet est introuvable.");
-    while (true);
+    Serial.println("Le module ethernet n'a pas été trouvé ");
+    while (true) {
+      delay(1); // ne fait rien, bloque le programme dans une boucle 
+    }
+  }
+  if (Ethernet.linkStatus() == LinkOFF) {
+    Serial.println("Pas de câble ethernet trouvé");
   }
   
-  /*if (Ethernet.begin(mac) == 0) {
-    Serial.println("Échec de l'initialisation Ethernet avec DHCP");
-    while (true); // Bloque ici
-  }
-  */
-  Serial.print("Adresse IP obtenue via DHCP : ");
-  Serial.println(Ethernet.localIP());
-}
+  Ethernet.begin(mac,ip);                    // Initialise l'ethernet avec l'adresse IP et l'adresse MAC définient.
+  while(!Udp.begin(UDPMessagingPort)){}      // Initialise l'UDP
+  delay(100);                                // On attend un petit peu pour être sûr que tout au eu le temps de démarrer 
 
-void loop() {}
+  Serial.print("connecté à : ");
+  IPAddress remote = Udp.remoteIP();
+    for (int i=0; i < 4; i++) {
+      Serial.print(remote[i], DEC);
+      if (i < 3) {
+        Serial.print(".");
+      }
+    }
+  Serial.println();
+
+
+}
