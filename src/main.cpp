e22bffdb64ee80497e98617bb9a2d99a22f2fb77
Benoit Borek <borekbenoit8@gmail.com>
2025-02-06 17:04:26 +0100

    Update main.cpp
    Mise à jour

--- src/main.cpp
+++ src/main.cpp
@@ -7,23 +7,23 @@ IDE : Visual Studio Code ver 1.96.2
 Constituants :
 
 Arduino Mega 2560
-Module ethernet WIZ811MJ
+Module ethernet WIZ810MJ
 
 
-Lien de la doc technique du module : https://docs.wiznet.io/img/products/wiz811mj/WIZ811MJ_DS_V120E.pdf
+Lien de la doc technique du module : https://www.signal.com.tr/pdf/cat/WIZ810MJ.pdf
 */
 
 
 
 #include <SPI.h>
 #include <Ethernet.h>
-#include <DHT.h>
+
 
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 IPAddress ip(192,168,1,20);
 IPAddress myDns(192, 168, 1, 1);
 IPAddress gateway(192, 168, 1, 1);
-IPAddress subnet(255, 255, 255, 0);
+IPAddress subnet(255, 255, 255, 0); 
 
 
 
@@ -33,7 +33,7 @@ void setup() {
   
 
   Serial.begin(9600);
-  Ethernet.init(10);
+  Ethernet.init(53);
   Serial.println("Initialisation Ethernet...");
 
   // Initialisation de l'Ethernet en utilisant la pin 53 pour CS
